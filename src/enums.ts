759d1c71e260c4fe8e97b092bde4121b76b683c7
@yevheniia_balda <Yevheniia_Balda@epam.com>
2023-10-13 21:39:12 +0300

    [HW2] refactor: Add enums for errors
    - added HttpStatusCodes  and ResponseMessages enums

---
+++ src/enums.ts
@@ -0,0 +1,9 @@
+export enum HttpStatusCodes {
+  NotFound = 404,
+  InternalServerError = 500,
+}
+
+export enum ResponseMessages {
+  NotFound = "Resource not found",
+  InternalServerError = "Internal Server Error",
+}
--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 
+import { ResponseMessages, HttpStatusCodes } from "../enums";
+
 export const notFoundHandler = (
   _req: Request,
   res: Response,
   _next: NextFunction,
 ): void => {
-  res.status(404).json({ message: "Resource not found" });
+  res
+    .status(HttpStatusCodes.NotFound)
+    .json({ message: ResponseMessages.NotFound });
 };
 
 export const errorHandler = (
@@ -15,5 +19,7 @@ export const errorHandler = (
   _next: NextFunction,
 ): void => {
   console.error(err.stack);
-  res.status(500).json({ message: "Internal Server Error" });
+  res
+    .status(HttpStatusCodes.InternalServerError)
+    .json({ message: ResponseMessages.InternalServerError });
 };
